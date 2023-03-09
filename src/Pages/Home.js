946c452803cd56c38a46ae9773f00ccc958e429f
Devanand Verma <devanandverma01d@gmail.come>
2023-03-09 14:32:12 +0530

    Creating new student from frontend... using CRUD operation

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,43 +1,9 @@
 import React from 'react'
-import { Button, Form, Table } from 'react-bootstrap'
-
 
 export default function Home() {
   return (
-    <div className='container mt-5'>
-       <h1 className='text-center'>Creating a Form</h1> 
-      <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Student's Name:</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-    <br />
-    <hr />
-    <br />
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>First Name</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>
-            <button className='btn btn-primary btn-sm me-1'>View</button>
-            <button className='btn btn-warning btn-sm me-1'>Edit</button>
-            <button className='btn btn-danger btn-sm me-1'>Delete</button>
-          </td>
-        </tr>
-      </tbody>
-    </Table>
+    <div>
+      Home
     </div>
   )
 }
--- src/Pages/Student/CreateStudent.js
+++ src/Pages/Student/CreateStudent.js
@@ -1,9 +1,90 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Button, Form, Table } from 'react-bootstrap'
 
-export default function CreateStudent() {
+
+let CreateStudent=()=>{
+  //1. Hooks Area
+  const[teacher,setTeacher]=useState([])
+  useEffect(()=>{
+    fetch('http://localhost:1337/api/teachers')
+    .then(res=>res.json())
+    .then(data=>{
+      console.log(data.data)
+      setTeacher(data.data)
+    })
+    .catch()
+  },[])
+  //2. Function definition area
+  let createNewStudent=()=>{
+    //alert("Hello")
+    let payload={
+      "data": {
+        "name": document.getElementById('student_name').value,
+        "teachers": [parseInt(document.getElementById('teacher').value)]
+      }
+    }
+    fetch(`http://localhost:1337/api/students`,{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify(payload)
+    })
+    .then(res=>res.json())
+    .then((data)=>{
+      alert("New Student Created Successfully...")
+      console.log(data)
+    })
+    .catch()
+    console.log(payload)
+  }
+  //3. Return statement
   return (
-    <div>
-      CreateStudent
+    <div className='container'>
+      <div className='container mt-5'>
+      <h1 className='text-center'>Creating a Form</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Select Teacher's Name:</Form.Label>
+        <Form.Select id="teacher" aria-label="Default select example">
+        {
+          teacher.map((cv,idx,arr)=>{
+            return <option key={idx} value={cv.id}>{cv.attributes.name}</option>
+          })
+        }
+          
+        </Form.Select>
+          <Form.Label>Student's Name:</Form.Label>
+          <Form.Control id="student_name" type="text" placeholder="Enter name" />
+        </Form.Group>
+      <Button variant="primary" type="button" onClick={createNewStudent}>Submit</Button>
+    </Form>
+    <br />
+    <hr />
+    <br />
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>First Name</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Devanand</td>
+          <td>
+            <button className='btn btn-primary btn-sm me-1'>View</button>
+            <button className='btn btn-warning btn-sm me-1'>Edit</button>
+            <button className='btn btn-danger btn-sm me-1'>Delete</button>
+          </td>
+        </tr>
+      </tbody>
+    </Table>
+      </div>
     </div>
+    
   )
 }
+export default CreateStudent;
